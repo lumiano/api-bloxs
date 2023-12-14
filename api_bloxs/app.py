b9c4121bb096ad9c82c04a786ce940d6ccfe92d5
lumiano <k_l.s@outlook.com>
2023-12-14 17:06:33 -0300

    chore: init poetry package manager, apiflask deps

---
+++ api_bloxs/app.py
@@ -0,0 +1,10 @@
+from apiflask import APIFlask
+
+app = APIFlask(__name__)
+
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
